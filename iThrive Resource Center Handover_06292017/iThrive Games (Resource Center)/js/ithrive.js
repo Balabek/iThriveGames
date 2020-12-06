d0bb2fa6c57dcbe5d05096f1e892325c3365b2e9
Balabek <vonbabayan@gmail.com>
2020-12-06 15:21:53 +0300

    Final version

---
+++ iThrive Resource Center Handover_06292017/iThrive Games (Resource Center)/js/ithrive.js
@@ -0,0 +1,49 @@
+/**
+ * Created by KRM on 3/27/17.
+ */
+
+$(document).ready(function(){
+
+    // Blog slider initiation and options
+    $('#blogSlider').slick({
+        dots: true,
+        autoplay: false,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    dots: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+
+
+
+
+
+});
